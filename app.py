9b911b98fa680038b0a4068757528fd8140d9b31
LarsKoelbel <136699088+LarsKoelbel@users.noreply.github.com>
2026-01-28 15:59:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def upload_push_event():
             payload,
             upsert=True
         )
-        print("Upload complete")
+        print(f"Upload complete for data {payload}")
         return jsonify({"upload-success": True}), 200
 
     except Exception as e:
